33b0517ce4fb6ccc2fa02fe20d203b529e27bfb3
Eder Sousa <edersousa@gmail.com>
2023-08-07 21:20:38 -0300

    Create docs-links.sql

---
+++ docs-links.sql
@@ -0,0 +1,7 @@
+/*
+Oracle  - https://docs.oracle.com/en/database/oracle/oracle-database/21/sqlrf/index.html
+MySQL - https://dev.mysql.com/doc/refman/8.0/en/
+MariaDB - https://mariadb.org/documentation/#entry-header
+PostgreSQL  - https://www.postgresql.org/docs/current/ddl.html
+SQL Server -https://learn.microsoft.com/pt-br/sql/sql-server/?view=sql-server-ver16
+*/
